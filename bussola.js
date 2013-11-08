38fcb8b7a3c8f1bfc8f8dd3264782504c3b1bc77
paulo <olivigeoarte@gmail.com>
2013-11-08 08:48:36 -0200

    ososo

--- bussola.js
+++ bussola.js
@@ -1,28 +1,26 @@
 // JavaScript Document
 var watchID = null;
 
-    // Wait for device API libraries to load
-    //
+    
+    
     document.addEventListener("deviceready", onDeviceReady, false);
 
-    // device APIs are available
-    //
+   
+    
     function onDeviceReady() {
         startWatch();
     }
 
-    // Start watching the compass
-    //
+   
     function startWatch() {
 
-        // Update compass every 3 seconds
+        
         var options = { frequency: 3000 };
 
         watchID = navigator.compass.watchHeading(onSuccess, onError, options);
     }
 
-    // Stop watching the compass
-    //
+    
     function stopWatch() {
         if (watchID) {
             navigator.compass.clearWatch(watchID);
@@ -30,15 +28,13 @@ var watchID = null;
         }
     }
 
-    // onSuccess: Get the current heading
-    //
+    
     function onSuccess(heading) {
         var element = document.getElementById('heading');
         element.innerHTML = 'Heading: ' + heading.magneticHeading;
     }
 
-    // onError: Failed to get the heading
-    //
+    
     function onError(compassError) {
         alert('Compass error: ' + compassError.code);
     }
